cbf674c481b9938df20e0d18591716b7eb3d5ded
Arnaud Le Blanc <arnaud.lb@gmail.com>
2020-10-10 17:03:17 +0200

    test

--- .github/workflows/push.sh
+++ .github/workflows/push.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -e
-pwd
-ls -l
-# rsync -av --delete output/php-chunked-xhtml/ 
+cd gh-pages
+rsync -av --delete ../php-rdkafka-doc/output/php-chunked-xhtml/ phpdoc/
+git add .
+git commit -m "Update to $GITHUB_SHA"
+git push origin HEAD
